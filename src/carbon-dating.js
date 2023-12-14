11b01754aa149e50189f275a73f1725696b9d7f4
Ivan Lavrinovich <atye996@gmail.com>
2023-12-14 20:30:08 +0300

    feat: finish carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(parseInt(sampleActivity)) || parseFloat(sampleActivity) > 15 || parseFloat(sampleActivity) <= 0) {
+    return false;
+  }
+  let x = Math.log(MODERN_ACTIVITY / sampleActivity);
+  let y = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(x / y);
 }
 
 module.exports = {
