b4709b4cc907f6945b6dbbcb1ea1f6ddebdad434
Ivan Lavrinovich <atye996@gmail.com>
2023-12-17 15:25:29 +0300

    feat: finish mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,9 +23,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = structuredClone(matrix);
+
+  function count(x, y, matrix) {
+    let count = 0;
+    for (let a = x - 1; a <= x + 1; a++) {
+      for (let b = y - 1; b <= y + 1; b++) {
+        if (a >=0 && b >= 0 && a < matrix.length && b < matrix[a].length) {
+              count += matrix[a][b] === true ? 1 : 0;
+        }
+      }
+    }
+    return matrix[x][y] ? --count : count;
+  };
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      result[i][j] = count(i, j, matrix);
+    }
+  }
+  return result;
 }
 
 module.exports = {
